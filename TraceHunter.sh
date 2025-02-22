3b7027a5b3f4699562e8bd6a4e5dd34362db800b
renancyber <renanzaira98@gmail.com>
2025-02-21 21:17:53 -0300

    Update TraceHunter.sh

--- TraceHunter.sh
+++ TraceHunter.sh
@@ -21,3 +21,32 @@ lsblk > $COLLECTED_DIR/disk_info.txt
 #Coleta de Conexões de Rede
 echo -e "\033[1;95m Coletando informações de rede...\033[0m"
 ss -tunap >  $COLLECTED_DIR/active_connections.txt
+netstat >> $COLLECTED_DIR/open_ports.txt
+
+echo -e "\033[1;95m Coletando listas de processos \033[0m"
+
+ps aux >> $COLLECTED_DIR/process_list.txt
+
+
+echo -e "\033[1;95m Coletando logs do sistema \033[0m"
+ 
+cp -r /var/log/syslog $COLLECTED_DIR/syslog.log
+cp -r /var/log/auth.log $COLLECTED_DIR/auth.log
+cp -r /var/log/dmesg $COLLLECTED_DIR/dmesg.log
+
+
+echo -e "\033[1;95m Coletando arquivos de configuração \033[0m"
+
+cp -r /etc $COLLECTED_DIR/backup/
+
+
+echo -e "\033[1;95m Listando o diretório raiz \033[0m"
+
+ls -l / >> $COLLECTED_DIR/root_dir_list.txt
+
+
+HOSTNAME=$(hostname)
+
+DATAHORA=$(date +"%Y_%m_%d_%H_%M_%S")
+
+tar -czf "TraceHunter_${HOSTNAME}_${DATAHORA}.tar.gz" $COLLECTED_DIR
